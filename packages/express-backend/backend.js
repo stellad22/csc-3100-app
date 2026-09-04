a4c42d4f65da6538fc3df1984b62a830aa71e730
Stella <stelladaoud22@gmail.com>
2026-09-04 14:49:51 -0700

    finished step 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,18 +33,16 @@ const users = {
   ],
 };
 
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
   } else {
-    res.send(users);
+    res.send(result);
   }
 });
 
