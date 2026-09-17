804df6f965d267a4353a7bff0ed8de279cd60a0a
Stella <stelladaoud22@gmail.com>
2026-09-16 23:11:13 -0700

    completed step 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
+import React, { useState, useEffect } from "react";
+
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -14,9 +28,37 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-  setCharacters([...characters, person]);
-  }
+ function updateList(person) {
+  postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+  function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+}
+
+function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+}
 
   return (
     <div className="container">
